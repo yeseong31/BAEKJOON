25cbc1c50a4be5478ffe631c1f4518ef087fc847
Yeseong31 <66625672+yeseong31@users.noreply.github.com>
2021-02-23 18:31:30 +0900

    피보나치 수열 - 코드 설명 추가

--- 10870_Fibonacci.py
+++ 10870_Fibonacci.py
@@ -8,3 +8,14 @@ def fibo(n):
     return fibo(n - 1) + fibo(n - 2)
 
 print(fibo(int(input())))
+
+# 재귀함수를 이용한 피보나치 수열 알고리즘은 굉장히 비효율적이라는 것을 알아둘 것.
+# '메모이제이션' 방법을 이용하면 더 효율적으로 알고리즘을 구현할 수 있다.
+# 다만, 메모리 측면에서는 비효율적일 수도 있다.
+
+n = int(input())
+d = [0] * (n + 1)
+d[1] = d[2] = 1
+for i in range(3, n + 1):
+    d[i] = d[i - 1] + d[i - 2]
+print(d[n])
